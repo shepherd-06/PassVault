eddd8a4a79ae5f923bc0486cd48de01485713c82
Ibtehaz <ibtehaz@finder-lbs.com>
2018-09-19 11:36:00 +0600

    dropbox endpoint added

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/dropbox')
+def dropbox():
+    return render_template('dropbox.html')
 
 if __name__ == '__main__':
     app.run()
