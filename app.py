f20d9007918d83010e6156ab8f01258783d96bf9
Ibtehaz <ibtehaz@finder-lbs.com>
2018-10-01 11:48:34 +0600

    postgresql database added

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+from models import Result
 
+#DATABASE_URL
 @app.route('/')
 def hello_world():
     return render_template('index.html')
