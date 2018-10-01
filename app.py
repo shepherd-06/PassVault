9162fd952c2481c9e972eeb53e8375fdcabfc568
Ibtehaz <ibtehaz@finder-lbs.com>
2018-10-01 17:34:46 +0600

    modified static, app.py, removed template

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
-from models import Result
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-from models import Result
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    result = Result('https://google.com', 'Hello World', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
-    print(result.__repr__)
+    from models import Result
+    result = Result('https://google.com', 'Hello World - {}'.format(datetime.now()), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    db.session.add(result)
+    db.session.commit()
+    # print(result.id)
+    # print(result.url)
     return render_template('index.html')
 
-@app.route('/dropbox')
-def dropbox():
-    return render_template('dropbox.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run()
